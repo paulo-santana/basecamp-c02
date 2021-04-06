defee423b5ac6b9ef260e7532e3f4ec13f174fc1
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-06 11:23:38 -0300

    09 - ft_strcapitalize

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,34 @@
+int		is_alphanumeric(char c)
+{
+	if ((c >= '0' && c <= '9') ||
+		(c >= 'A' && c <= 'Z') ||
+		(c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
+
+int		is_lower_alpha(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || !is_alphanumeric(str[i - 1]))
+		{
+			if (is_lower_alpha(str[i]))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		i++;
+	}
+	return str;
+}
